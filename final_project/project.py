8fcdc44106c35696c627fac5cbbc2f0c88b2438b
laura <lt.martinez.2018@alumnos.urjc.es>
2023-12-25 12:03:26 +0000

    <mod> it finally works. left exploring what feats to use

--- final_project/project.py
+++ final_project/project.py
@@ -1,37 +1,40 @@
 from pyspark.sql import *
+from pyspark.sql.functions import *
 from pyspark import SparkContext
 from functools import reduce
-from pyspark.ml.feature import StringIndexer, VectorAssembler, LinearRegression
-
+from pyspark.ml.feature import Normalizer, StringIndexer, VectorAssembler
+from pyspark.ml import Pipeline
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.evaluation import RegressionEvaluator
+import click 
 
 import bz2
 import os
 
 ## Functions ######################################
 
-# Initialize SparkSession
-def init_spark():
-	sc = SparkContext("local", "ComercialFlights")
-	spark = SparkSession.builder \
-				.appName("First Session") \
-				.master("local[*]") \
-				.getOrCreate()
-
-	sc.setLogLevel("ERROR")
-
 # Load data into pyspark dataframe
-def load_data():
+def load_data(path):
+    # Initialize SparkSession
+    sc = SparkContext("local", "ComercialFlights")
+    spark = SparkSession.builder \
+                .appName("First Session") \
+                .master("local[*]") \
+                .getOrCreate()
+
+    sc.setLogLevel("ERROR")
+
     # read all csv files in the directory to pyspark dataframe
-	csv_files = os.listdir("../BigData/data/project_data/")
-	df_pyspark =[]
-	for file in csv_files:
-		file_path = "../BigData/data/project_data/" + file
-		df = spark.read.csv(file_path, header=True, inferSchema=True)
-		df_pyspark.append(df)
-	
-	# merge all dataframes into one
-	df_pyspark = reduce(DataFrame.unionAll, df_pyspark)
-	return df_pyspark
+    csv_files = os.listdir(path)
+    df_pyspark =[]
+    for file in csv_files:
+        file_path = path + file
+        df = spark.read.csv(file_path, header=True, inferSchema=True)
+        df_pyspark.append(df)
+
+    # merge all dataframes into one
+    df_pyspark = reduce(DataFrame.unionAll, df_pyspark)
+    return df_pyspark
 
 # Rename columns
 def edit_column_names(df):
@@ -61,22 +64,7 @@ def edit_column_names(df):
         df = df.withColumnRenamed(col, col.lower())
     return df
 
-# select columns:
-def my_columns (df):
-    df = df.select('year','month','day_of_month', 'day_of_week', 'actual_departure_time',
-                   'scheduled_departure_time', 'scheduled_arrival_time', 'airline_code',
-                   'flight_number', 'plane_number', 'scheduled_flight_time', 'arrival_delay',
-                   'departure_delay', 'origin', 'dest', 'distance', 'taxi_out', 'cancelled',
-                   'cancellation_code')
-    return df
-
-# combine to create dates:
-def add_date_column(df):
-    df = df.withColumn('date', to_date(concat(col('day_of_month'), lit(' '),
-                                              col('month'), lit(' '), col('year')), 'd M yyyy'))
-    return df
-
-# some strings to float:
+# String values to float:
 def string_to_float(df):
     df = df.withColumn('arrival_delay', col('arrival_delay').cast('float'))
     df = df.withColumn('departure_delay', col('departure_delay').cast('float'))
@@ -84,16 +72,15 @@ def string_to_float(df):
     df = df.withColumn('distance', col('distance').cast('float'))
     return df
 
-# encode categorical features:
+# Encode categorical features:
 def encode_categorical_features(df):
     indexer = StringIndexer(inputCols=['airline_code', 'origin', 'dest', 'cancellation_code', 'plane_number'],
                             outputCols=['airline_index', 'origin_index', 'dest_index', 'cancellation_index', 'plane_index'])
     
     df = indexer.fit(df).transform(df)
     return df
-    
 
-# convert time to minutes:
+# Convert time to minutes:
 def convert_time_to_minutes(df):
     # for actual_departure_time, scheduled_departure_time, scheduled_arrival_time, scheduled_flight_time transform to minutes
     # take the first two digits and multiply by 60 and add the last two digits
@@ -113,85 +100,112 @@ def convert_time_to_minutes(df):
     
     return df
 
-# handle missing values:
-def handle_missing_values(df):
-	# eliminar filas donde actual_departure_time es null
-	df = df.filter(df.actual_departure_time.isNotNull())
-	# eliminar filas donde scheduled_flight_time es null
-	df = df.filter(df.scheduled_flight_time.isNotNull())
-
-	return df
-
+# Keep only relevant columns:
 def my_df(df):
-	# select columns
-	df = df.select('date','year','month','day_of_month', 'day_of_week', 'actual_departure_time_mins',
-					'scheduled_departure_time_mins', 'scheduled_arrival_time_mins', 'airline_index',
-					'flight_number', 'scheduled_flight_time_mins', 
-					'origin_index', 'dest_index', 'distance', 'taxi_out', 'cancelled', 'departure_delay')
+    # select columns
+    df = df.select('year', 'month', 'day_of_month', 'day_of_week', 'actual_departure_time_mins',
+ 					'scheduled_departure_time_mins', 'scheduled_arrival_time_mins', 'airline_index',
+ 					'flight_number', 'scheduled_flight_time_mins', 'departure_delay',
+ 					'origin_index', 'dest_index', 'distance', 'cancelled',
+ 					'arrival_delay')
+    return df
 
+# Handle null values:
+def drop_nulls(df):
+	# remove rows in arrival_delay where arrival_delay is null
+	df = df.filter(df.arrival_delay.isNotNull())
+	# remove rows in scheduled_flight_time_mins where departure_delay is null
+	df = df.filter(df.scheduled_flight_time_mins.isNotNull())
+	# remove rows in distance where distance is null
+	df = df.filter(df.distance.isNotNull())
 	return df
-    
+
+# Drop cancelled flights:
+def drop_cancelled(df):
+    df = df.filter(df.cancelled == 0)
+    return df
+
 
 # standarize df
 def standarize_dataframe(df):
     temp = edit_column_names(df)
-    temp = my_columns(temp)
     temp = string_to_float(temp)
-    temp = add_date_column(temp)
     temp = encode_categorical_features(temp)
     temp = convert_time_to_minutes(temp)
-    temp = handle_missing_values(temp)
     temp = my_df(temp)
+    temp = drop_nulls(temp)
+    temp = drop_cancelled(temp)
 
     return temp
 
-# vectorize features
-def vectorize_features(df, features):
-	featureassemble = VectorAssembler(inputCols=[features], outputCol='ind_features')
-	df = featureassemble.transform(df)
-	return df
+# Create model
+def create_model(df, my_features):
+    # Train-test split
+    train, test = df.randomSplit([0.7, 0.3], seed=42)
+    # Vectorize features
+    featureassmebler = VectorAssembler(inputCols=my_features, outputCol='features')
+	# Normlizer
+    normalizer = Normalizer(inputCol="features", outputCol="features_norm", p=1.0)
+    # Linear Regression
+    lr = LinearRegression(featuresCol='features_norm', labelCol='arrival_delay', maxIter=10, regParam=0.3, elasticNetParam=0.8)
+    # Pipeline
+    pipeline = Pipeline(stages=[featureassmebler, normalizer, lr])
+    # Fit pipeline
+    model = pipeline.fit(train)
+    
+    return train, test, model
 
+# Validate model
+def validate_model(model, test_data):
+    predictions = model.transform(test_data)
+    evaluator = RegressionEvaluator(labelCol="arrival_delay", predictionCol="prediction", metricName="mae")
+    mae = evaluator.evaluate(predictions)
+    print("MAE = %g" % mae)
+
+    evaluator = RegressionEvaluator(labelCol="arrival_delay", predictionCol="prediction", metricName="mse")
+    mse = evaluator.evaluate(predictions)
+    print("MSE = %g" % mse)
+
+    evaluator = RegressionEvaluator(labelCol="arrival_delay", predictionCol="prediction", metricName="rmse")
+    rmse = evaluator.evaluate(predictions)
+    print("RMSE = %g" % rmse)
+
+    evaluator = RegressionEvaluator(labelCol="arrival_delay", predictionCol="prediction", metricName="r2")
+    r2 = evaluator.evaluate(predictions)
+    print("R2 = %g" % r2)
+    
 #############################################
 
-def main():
-	# Initialize SparkSession
-	init_spark()
-	# load data into pyspark dataframe
-	df_pyspark = load_data()
-	
-	print('Number of rows: ', df_pyspark.count())
-	print('Number of columns: ', len(df_pyspark.columns))
-	
-	# Process data
-	df = standarize_dataframe(df_pyspark)
- 
-	# Vectorize features
-	# independent variable = arrival_delay (exclude it from features)
-	vectorized = vectorize_features(df, df.columns[:-1]) ####ESTO ESTÁ MAL, NO ESTA QUITANDO LA ARRIVAL_DELAY --> REVISAR
-	final_data = vectorized.select('ind_features', 'arrival_delay') # select variables we want to use
-	
-	# Split data into train and test
-	train_data, test_data = final_data.randomSplit([0.7, 0.3])
-	regressor = LinearRegression(featuresCol='ind_features', labelCol='arrival_delay')
-	model = regressor.fit(train_data)
-	print('Coefficients: ', model.coefficients)
-	print('Intercept: ', model.intercept)
- 
-	# Evaluate model
-	pred_results = model.evaluate(test_data)
-	print('RMSE: ', pred_results.rootMeanSquaredError)
-	print('MSE: ', pred_results.meanSquaredError)
-	print('R2: ', pred_results.r2)
-	print('MAE: ', pred_results.meanAbsoluteError)
- 
-	# 
- 
-	
- 
+@click.command()
+@click.option('--data_path', 
+              default='BigData/data/project_data/', 
+              help='Path to the data folder')
+
+def main(data_path):
+    click.echo('Loading data from {}'.format(data_path))
     
+    # Load data into pyspark dataframe
+    df_pyspark = load_data(data_path)
 
+    print('Initial number of rows: ', df_pyspark.count())
+    print('initial number of columns: ', len(df_pyspark.columns))
 
+    # Process data
+    df = standarize_dataframe(df_pyspark)
+    print('Final number of rows: ', df.count())
+    print('Final number of columns: ', len(df.columns))
 
+    # Create model
+    my_features = ['year', 'month', 'day_of_month', 'day_of_week', 'actual_departure_time_mins',
+                    'scheduled_departure_time_mins', 'scheduled_arrival_time_mins', 'airline_index',
+                    'flight_number', 'scheduled_flight_time_mins', 'departure_delay',
+                    'origin_index', 'dest_index', 'distance']
+
+    print('Number of features: ', len(my_features))
+    train, test, model = create_model(df, my_features)
+
+    # Evaluate model
+    validate_model(model, test)
 
 if __name__ == "__main__":
 	main()
